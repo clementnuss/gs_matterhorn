f796bd8cef9e4f81a497dc9fd0e9735a0f07a666
Clément Nussbaumer <clement@astutus.org>
2017-10-03 18:54:49 +0200

    Implement GoogleTest framework
    The ground_station folder contains the code of the program. It is called/used by the main.cpp that will be the entry point of the software.
    For the tests: ths gs_tests folder will contain all test suites. `gs_tests/CMakeLists.txt` takes care of installing the Google Test framework.
    `basic_tests` contains the tests (every .cpp file in this folder will be included by CMake, after a CMake reload).
    One can see that inside `gs_tests/basic_tests/CMakeLists.txt` the executable created from the sources is linked with the `ground_station` library.

---
+++ ground_station/General.cpp
@@ -0,0 +1,10 @@
+//
+// Created by clement on 03/10/2017.
+//
+
+#include "General.h"
+
+
+int simple_test_function(int arg) {
+    return arg + 14;
+}
--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
+#include <General.h>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
 
-int simple_test_function(int arg) {
-    return arg + 14;
+    int result = simple_test_function(0);
+    std::cout << "The result is : " << result << std::endl;
+    return 0;
 }
