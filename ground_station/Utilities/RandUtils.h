379661b3006455d68e7c9cd4d4ac23318a504766
lkieliger <leandro.kieliger@epfl.ch>
2017-11-03 09:32:14 +0100

    Comment RandUtils

--- ground_station/Utilities/RandUtils.h
+++ ground_station/Utilities/RandUtils.h
@@ -10,11 +10,18 @@
 // Adapted from https://stackoverflow.com/questions/22895130/creating-full-range-of-random-floats-using-stdrandom
 // and https://stackoverflow.com/questions/32907254/uniform-random-distribution-base-class-for-both-int-and-double
 
+/**
+ * Specify the distribution type depending on the type of the parameter type T.
+ */
 template<typename T>
 using distributionType = std::conditional_t<std::is_integral<T>::value,
         std::uniform_int_distribution<T>,
         std::uniform_real_distribution<T>>;
 
+/**
+ * A class generating uniformly distributed random numbers whose types match the parameter type T.
+ * @tparam T
+ */
 template<typename T>
 class Rand {
 public:
