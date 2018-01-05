2851411229be67f21b549b6d5776c694bbf4e263
lkieliger <leandro.kieliger@epfl.ch>
2018-01-05 21:52:24 +0100

    Add Qt test example

---
+++ qt_tests/DummyTests.cpp
@@ -0,0 +1,8 @@
+
+#include "DummyTests.h"
+
+QTEST_MAIN(DummyTests)
+
+void DummyTests::testsAreCorrectlySetup() {
+    QVERIFY(true);
+}
---
+++ qt_tests/DummyTests.h
@@ -0,0 +1,15 @@
+#ifndef GS_MATTERHORN_DUMMYTESTS_H
+#define GS_MATTERHORN_DUMMYTESTS_H
+
+
+#include <QtTest/QtTest>
+
+class DummyTests : public QObject {
+Q_OBJECT
+private slots:
+
+    void testsAreCorrectlySetup();
+};
+
+
+#endif //GS_MATTERHORN_DUMMYTESTS_H
