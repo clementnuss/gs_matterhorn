8975f683ef1b551fcfa4ee81b89670fab3e2d6b7
lkieliger <leandro.kieliger@epfl.ch>
2018-01-21 23:05:04 +0100

    Implement interval checks for union

--- ground_station/DataStructures/Interval2D.h
+++ ground_station/DataStructures/Interval2D.h
@@ -5,6 +5,7 @@
 
 #include <boost/icl/interval.hpp>
 
+// Heavily inspired from
 class Interval2D {
 public:
 
@@ -22,6 +23,34 @@ public:
                boost::icl::contains(longitudeInterval_, longitudeIndex);
     }
 
+    int size() {
+        return boost::icl::size(latitudeInterval_) * boost::icl::size(longitudeInterval_);
+    }
+
+    int sizeOfIntersectionWith(const Interval2D &that) {
+        return boost::icl::size(this->latitudeInterval_ & that.latitudeInterval_) *
+               boost::icl::size(this->longitudeInterval_ & that.longitudeInterval_);
+    }
+
+    Interval2D boundingUnion(const Interval2D &that) {
+        return Interval2D(
+                boost::icl::hull(this->latitudeInterval_, that.latitudeInterval_),
+                boost::icl::hull(this->longitudeInterval_, that.longitudeInterval_)
+        );
+    }
+
+    bool isUnionableWith(Interval2D that) {
+        int unionSize = this->size() + that.size() - this->sizeOfIntersectionWith(that);
+        return boundingUnion(that).size() == unionSize;
+    }
+
+    Interval2D unionWith(const Interval2D &that) {
+        if (!isUnionableWith(that)) {
+            throw std::invalid_argument("These intervals are not unionable");
+        }
+        return boundingUnion(that);
+    }
+
     int startLongitude() const {
         return longitudeInterval_.lower();
     }
@@ -36,14 +65,6 @@ public:
 
     int endLatitude() const {
         return latitudeInterval_.upper();
-    }
-
-    //TODO: check unionable before
-    Interval2D unionWith(const Interval2D &that) {
-        return Interval2D(
-                boost::icl::hull(this->latitudeInterval_, that.latitudeInterval_),
-                boost::icl::hull(this->longitudeInterval_, that.longitudeInterval_)
-        );
     }
 
 private:
